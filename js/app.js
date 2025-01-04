de462b514c736d2e5632cef818f357f1bf3e6ef6
Jemster07 <emelie.kerek@gmail.com>
2025-01-04 16:34:12 -0500

    started building time tracking logic

--- js/app.js
+++ js/app.js
@@ -1,7 +1,21 @@
 $(document).ready(function () {
-
+    count_time();
 });
 
-function countTime(){
-    setInterval(() => console.log(new Date().toLocaleTimeString()),1000);
+function count_time(){
+    setInterval(update_clock_fields, 1000);
+}
+
+function update_clock_fields(){
+    $('#time_keeper').val(new Date().toLocaleTimeString());
+
+    let timeArray = $('#time_keeper').val().split(" ");
+    timeArray = timeArray[0].split(":");
+    let minutes = timeArray[1];
+
+    $('#interval_tracker').val(minutes);
+
+    if(minutes % 15 == 0){
+        console.log("15 minutes has passed");
+    }
 }
