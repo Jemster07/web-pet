657cbfbb7b6a6b9aa74ff925739e2e4152e4ecd4
Jemster07 <emelie.kerek@gmail.com>
2025-01-04 23:06:29 -0500

    actually got the clock logic working

--- js/app.js
+++ js/app.js
@@ -1,19 +1,44 @@
-let countTime = setInterval(update_clock_fields, 1000);
+var trigger = true;
+var reinit = false;
+var countTime = setInterval(update_clock_fields, 1000);
 
-function update_clock_fields(){
+function update_clock_fields(){    
     $('#time_keeper').val(new Date().toLocaleTimeString());
 
-    let timeArray = $('#time_keeper').val().split(" ");
+    var timeArray = $('#time_keeper').val().split(" ");
     timeArray = timeArray[0].split(":");
-    let minutes = timeArray[1];
-    
-    if(minutes % 5 == 0 || minutes == '00'){
-        reinitializeClock();
+    var minutes = timeArray[1];
+
+    if(minutes % 5 == 0){
+        update_status(minutes);
+    }
+
+    if((minutes % 5 != 0) && trigger == false){
+        trigger = true;
     }
 }
 
-function reinitializeClock(){
-    clearInterval(countTime);
-    console.log('Clock reinitialized');
+function update_status(minutes){
+    // Require minutes to be divisible by 5 to prevent scenarios where update_status runs before previous instance completes
+    while(trigger && (minutes % 5 == 0)){
+        // Make trigger false to prevent re-running functions while they are still executing
+        trigger = false;
+
+        // Generate random number, passing in minutes
+        // Execute status functions, passing in random number
+        
+        // Stop the interval execution
+        clearInterval(countTime);
+        reinit = true;
+        break;
+    }
+
+    if(reinit){
+        reinit = false;
+        reinitialize_clock();
+    }
+}
+
+function reinitialize_clock(){
     countTime = setInterval(update_clock_fields, 1000);
 }
