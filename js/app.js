d89502ab4dc5aeea9e1facb4a97cb7a7c4d45982
Jemster07 <emelie.kerek@gmail.com>
2025-01-05 13:10:01 -0500

    added more validation and scoped variables

--- js/app.js
+++ js/app.js
@@ -1,44 +1,49 @@
-var trigger = true;
-var reinit = false;
-var countTime = setInterval(update_clock_fields, 1000);
-
-function update_clock_fields(){    
-    $('#time_keeper').val(new Date().toLocaleTimeString());
-
-    var timeArray = $('#time_keeper').val().split(" ");
-    timeArray = timeArray[0].split(":");
-    var minutes = timeArray[1];
-
-    if(minutes % 5 == 0){
-        update_status(minutes);
+$(document).ready(function(){
+    // Clock Logic
+    var trigger = true;
+    var reinit = false;
+    var countTime = setInterval(update_time_keeper, 1000);
+    
+    function update_time_keeper(){    
+        $('#time_keeper').val(new Date().toLocaleTimeString());
+    
+        let timeArray = $('#time_keeper').val().split(" ");
+        timeArray = timeArray[0].split(":");
+        let minutes = timeArray[1];
+    
+        if((minutes % 5 == 0) && trigger == true){
+            update_status(minutes);
+        }
+    
+        if((minutes % 5 != 0) && trigger == false){
+            trigger = true;
+        }
+    }
+    
+    function update_status(minutes){
+        while((minutes % 5 == 0) && trigger){
+            trigger = false;
+    
+            // Generate random number, passing in minutes
+            // Execute status functions, passing in random number
+            
+            clearInterval(countTime);
+            reinit = true;
+            break;
+        }
+    
+        if(reinit){
+            reinit = false;
+            reinitialize_clock();
+        }
+    }
+    
+    function reinitialize_clock(){
+        countTime = setInterval(update_time_keeper, 1000);
     }
 
-    if((minutes % 5 != 0) && trigger == false){
-        trigger = true;
-    }
-}
-
-function update_status(minutes){
-    // Require minutes to be divisible by 5 to prevent scenarios where update_status runs before previous instance completes
-    while(trigger && (minutes % 5 == 0)){
-        // Make trigger false to prevent re-running functions while they are still executing
-        trigger = false;
-
-        // Generate random number, passing in minutes
-        // Execute status functions, passing in random number
-        
-        // Stop the interval execution
-        clearInterval(countTime);
-        reinit = true;
-        break;
-    }
-
-    if(reinit){
-        reinit = false;
-        reinitialize_clock();
-    }
-}
-
-function reinitialize_clock(){
-    countTime = setInterval(update_clock_fields, 1000);
-}
+    // Warn user before closing tab
+    // window.onbeforeunload = function(e) {
+    //     return '';
+    // };
+});
